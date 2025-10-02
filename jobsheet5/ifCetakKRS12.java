e0694f812851958b9414871317c2d61afe44e593
“ghafinfr” <ghafinfr@gmail.com>
2025-10-02 07:52:37 +0700

    modifikasi percobaan 1

--- jobsheet5/ifCetakKRS12.java
+++ jobsheet5/ifCetakKRS12.java
@@ -12,7 +12,10 @@ public class ifCetakKRS12 {
 
         if (uktLunas) {
             System.out.println("Pembayaran UKT terverifikasi ");
-            System.out.println("Silakan cetak KRS dan minta tanda tangan DPA ");       
+            System.out.println("Silakan cetak KRS dan minta tanda tangan DPA "); 
+        } else {
+        
+            System.out.println("Registrasi ditolak,Silakan Lunasi UKT Terlebih dahulu");
     }
     sc.close();
     }
